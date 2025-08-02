bdaf0a67ec29607271c2ca09c5126a9470f6f975
Michael Whittemore <michaelwhittemore@gmail.com>
2025-08-02 12:47:36 -0700

    tslint seems to be working

--- src/typescript/gameLoop.ts
+++ src/typescript/gameLoop.ts
@@ -1,9 +1,11 @@
 // I think we should create a customizable interval. 
 // Need a gameTick method which is called every interval
 
+// const badArray: Array<string> = ['a', 'b', 1]
+// const anotherBadArray: Array<number> = [1,2, 'as'] 
 
 const gameLoop = {
-    init: (interval = 1000) => {
+    init: (interval: number = 1000) => {
         console.log('gameLoop init called with interval = ', interval)
     }
 }
