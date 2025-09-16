971c5df1e449b4b90f1815aa68fb605bd77f4dee
Michael Whittemore <michaelwhittemore@gmail.com>
2025-09-16 11:25:15 -0700

    starting on adding split view to the mine

--- src/components/MineComponents/MineLocation.vue
+++ src/components/MineComponents/MineLocation.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import LocationComponent from '../UtilityComponents/LocationComponent.vue';
 import MineResourceButton from './MineResourceButton.vue';
+import ProspectForResourcesButton from './ProspectForResourcesButton.vue';
 </script>
 <template>
     <LocationComponent :location-name="'Mine'">
         <MineResourceButton :resource-type="'ore'" />
+        <ProspectForResourcesButton />
     </LocationComponent>
 </template>
---
+++ src/components/MineComponents/ProspectForResourcesButton.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { gameStateStorage } from '@/typescript/gameStateStorage';
+// maybe we should have the same split functionality as we do with ship and shop?
+</script>
+<template> 
+    ProspectForResourcesButton
+</template>
