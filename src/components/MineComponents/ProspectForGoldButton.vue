70151c2b4c95992fba03826e83e3acf8e5546646
Michael Whittemore <michaelwhittemore@gmail.com>
2025-09-20 12:33:29 -0700

    in the middle of working on propsecting for gold

--- src/components/MineComponents/ProspectForGoldButton.vue
+++ src/components/MineComponents/ProspectForGoldButton.vue
@@ -1,26 +1,30 @@
 <script setup lang="ts">
 import LoadingStateComponent from '../UtilityComponents/LoadingStateComponent.vue';
+import { computed } from 'vue';
+import { earnExperienceInSkill, activateNarrativeTrigger } from '@/typescript/gameHelpers';
 // HERE!
 // I should look to SearchForNewResource for some example
-// Hmm is it time dependant or amount depedant?
-// I'm thinking amount?
+// should add popup text on success. (while we're at it, add that to the search as well)
+
 // and we do amount as rng and as a function of prospectng level
 // this will need to consume oil at some point, but not yet
 // it needs to give xp
-// We will use loadingStateComponent again
-// need to have the button appear in the miningTab
-// Will need to track gold vein in the gameStateStorage
-// Maybe we should start with the mineVeinButton?
+
 const onEvent = () => {
+  earnExperienceInSkill('prospecting', 3)
   console.log('done!')
+  // WIll need to calculate how much gold to add to the vein
+  // will then need to update the game state
   //
 }
 const myString = "i'm the gold prospecting button, I don't require any oil at the moment"
-const shouldDisable = false; // has resources plus doesn't have active vein
+const computedShouldDisable = computed(() => {
+  return false
+}); // has resources plus doesn't have active vein
 const duration = 3;
 </script>
 <template>
-<LoadingStateComponent :button-text="myString" :duration="duration" :should-disable="shouldDisable"
+<LoadingStateComponent :button-text="myString" :duration="duration" :should-disable="computedShouldDisable"
 @finished-loading="onEvent"/>
 
 </template>
