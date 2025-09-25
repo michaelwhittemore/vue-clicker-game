59fca07ce942fc8c2056df2c45cb6c409aea216a
Michael Whittemore <michaelwhittemore@gmail.com>
2025-09-25 09:58:05 -0700

    added component, need to dynamically disaply and possibkly refactor how we deal with unlocked resoruces

---
+++ src/components/MineComponents/ExtractOilButton.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+// Will need to render this conditionally
+// WIll also need to add oil to unlocked in game state storage. - This may cause problems in SearchForNewResource.vue
+// Maybe it makes more sense to replace `unlockedResources` with `lockedResources`
+// In search for new resources we will need to iterate over the lockedResources array
+</script>
+<template>
+I'm the extract oil button
+</template>
--- src/components/MineComponents/MiningTab.vue
+++ src/components/MineComponents/MiningTab.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import MineOreButton from './MineOreButton.vue';
 import MineGoldVein from './MineGoldVein.vue';
-
+import ExtractOilButton from './ExtractOilButton.vue';
 </script>
 <template>
   <div class="miningColor selectableTab">
     <MineOreButton :resource-type="'ore'" />
+    <ExtractOilButton />
     <MineGoldVein />
   </div>
 
