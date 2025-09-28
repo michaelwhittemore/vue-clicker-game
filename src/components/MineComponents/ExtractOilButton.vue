55b074a52feffbb6982e4170e49486e40cbd1a6a
Michael Whittemore <michaelwhittemore@gmail.com>
2025-09-28 14:16:54 -0700

    encountered an issue with oil due to getResourceIncomeFactory

--- src/components/MineComponents/ExtractOilButton.vue
+++ src/components/MineComponents/ExtractOilButton.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { gameStateStorage } from '@/typescript/gameStateStorage';
-// Will need to render this conditionally
+import { earnExperienceInSkill, activateNarrativeTrigger } from '@/typescript/gameHelpers';
+
+import PopUpText from '../UtilityComponents/PopUpText.vue';
+
+const wasClickedTrigger = ref(false);
+const onClickHandler = () => {
+  console.log('oil')
+  gameStateStorage.resources.oil += 1;
+}
+// HERE! need to address getResourceIncomeFactory, that's what is causing a bug
+
 // WIll also need to add oil to unlocked in game state storage. - This may cause problems in SearchForNewResource.vue
 // Maybe it makes more sense to replace `unlockedResources` with `lockedResources`
 // In search for new resources we will need to iterate over the lockedResources array
 </script>
 <template>
-  <button v-if="gameStateStorage.unlockedResources.includes('oil')">
+  <button v-if="gameStateStorage.unlockedResources.includes('oil')" @click="onClickHandler">
     I'm the extract oil button
   </button>
 
